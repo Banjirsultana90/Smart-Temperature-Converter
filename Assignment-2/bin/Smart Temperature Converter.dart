bc0a9da17ebb7c21857a6e3c5064dbe6ff690144
unknown <banjirsultana15@gmail.com>
2026-09-02 13:25:11 +0600

    first commit

--- Assignment-2/bin/Smart Temperature Converter.dart
+++ Assignment-2/bin/Smart Temperature Converter.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
 
 void main() {
-  String again = 'y';
-
-  while (again.toLowerCase() == 'y') {
     print("====================================");
     print("       SMART TEMPERATURE CONVERTER");
     print("====================================");
@@ -16,54 +12,6 @@ void main() {
     print("6. Kelvin to Fahrenheit");
     print("7. Exit");
 
-    stdout.write("Enter your choice: ");
-    int choice = int.parse(stdin.readLineSync()!);
-
-    if (choice == 7) {
-
-    }
-
-    stdout.write("Enter temperature: ");
-    double temperature = double.parse(stdin.readLineSync()!);
-
-    switch (choice) {
-      case 1:
-        double result = (temperature * 9 / 5) + 32;
-        print("$temperature°C = ${result.toStringAsFixed(2)}°F");
-
-
-      case 2:
-        double result = (temperature - 32) * 5 / 9;
-        print("$temperature°F = ${result.toStringAsFixed(2)}°C");
-
-
-      case 3:
-        double result = temperature + 273.15;
-        print("$temperature°C = ${result.toStringAsFixed(2)} K");
-
-
-      case 4:
-        double result = temperature - 273.15;
-        print("$temperature K = ${result.toStringAsFixed(2)}°C");
-
-      case 5:
-        double result = (temperature - 32) * 5 / 9 + 273.15;
-        print("$temperature°F = ${result.toStringAsFixed(2)} K");
-
-
-      case 6:
-        double result = (temperature - 273.15) * 9 / 5 + 32;
-        print("$temperature K = ${result.toStringAsFixed(2)}°F");
-
-
-      default:
-        print("Invalid choice!");
-    }
-
-    stdout.write("Do you want to perform another conversion? (y/n): ");
-    again = stdin.readLineSync()!;
-    print("");
-  }
 
   print("====================================");
   print("          Thank You!");
