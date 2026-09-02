dc9364ad64aea4c0f79d3d9bb319e561ef9be458
unknown <banjirsultana15@gmail.com>
2026-09-02 19:14:12 +0600

    Add menu and switch statement

--- Assignment-2/bin/Temparature converter.dart
+++ Assignment-2/bin/Temparature converter.dart
@@ -1,4 +1,76 @@
-
+// import 'dart:io';
+//
+//  main() {
+//   String another = 'y';
+//
+//   while (another== 'y') {
+//     print("====================================");
+//     print("       SMART TEMPERATURE CONVERTER");
+//     print("====================================");
+//
+//     print("1. Celsius to Fahrenheit");
+//     print("2. Fahrenheit to Celsius");
+//     print("3. Celsius to Kelvin");
+//     print("4. Kelvin to Celsius");
+//     print("5. Fahrenheit to Kelvin");
+//     print("6. Kelvin to Fahrenheit");
+//     print("7. Exit");
+//
+//     stdout.write("Enter your choice: ");
+//     int choice = int.parse(stdin.readLineSync()!);
+//
+//     if (choice == 7) {
+//
+//     }
+//
+//     stdout.write("Enter temperature: ");
+//     double temperature = double.parse(stdin.readLineSync()!);
+//
+//     switch (choice) {
+//       case 1:
+//         double result = (temperature * 9 / 5) + 32;
+//         print("$temperature°C = ${result.toStringAsFixed(2)}°F");
+//
+//
+//       case 2:
+//         double result = (temperature - 32) * 5 / 9;
+//         print("$temperature°F = ${result.toStringAsFixed(2)}°C");
+//
+//
+//       case 3:
+//         double result = temperature + 273.15;
+//         print("$temperature°C = ${result.toStringAsFixed(2)} K");
+//
+//
+//       case 4:
+//         double result = temperature - 273.15;
+//         print("$temperature K = ${result.toStringAsFixed(2)}°C");
+//
+//
+//       case 5:
+//         double result = (temperature - 32) * 5 / 9 + 273.15;
+//         print("$temperature°F = ${result.toStringAsFixed(2)} K");
+//
+//
+//       case 6:
+//         double result = (temperature - 273.15) * 9 / 5 + 32;
+//         print("$temperature K = ${result.toStringAsFixed(2)}°F");
+//
+//
+//       default:
+//         print("Invalid choice!");
+//     }
+//
+//     stdout.write("Do you want to perform another conversion? (y/n): ");
+//     another = stdin.readLineSync()!;
+//     print("");
+//   }
+//
+//   print("====================================");
+//   print("          Thank You!");
+//   print("       Program Ended");
+//   print("====================================");
+// }
 
 void main(){
   print("====================================");
@@ -19,11 +91,44 @@ switch(choice){
   case 1:
         double result = (temperature * 9 / 5) + 32;
         print("$temperature°C = ${result.toStringAsFixed(2)}°F");
+  case 2:
+        double result = (temperature - 32) * 5 / 9;
+        print("$temperature°F = ${result.toStringAsFixed(2)}°C");
 
-}
+
+      case 3:
+        double result = temperature + 273.15;
+        print("$temperature°C = ${result.toStringAsFixed(2)} K");
+
+
+      case 4:
+        double result = temperature - 273.15;
+        print("$temperature K = ${result.toStringAsFixed(2)}°C");
+
+
+      case 5:
+        double result = (temperature - 32) * 5 / 9 + 273.15;
+        print("$temperature°F = ${result.toStringAsFixed(2)} K");
+
+
+      case 6:
+        double result = (temperature - 273.15) * 9 / 5 + 32;
+        print("$temperature K = ${result.toStringAsFixed(2)}°F");
+
+
+      default:
+        print("Invalid choice!");
+    }
 
 
 
+
+
+
+  print("====================================");
+  print("          Thank You!");
+  print("       Program Ended");
+  print("====================================");
 
 
 
